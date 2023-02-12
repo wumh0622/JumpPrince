66829bbe0818cda5d0f6af4631573eb71c777eee
MengHung <wumh2582@gmail.com>
2023-02-12 18:06:08 +0800

    背景音樂

--- Assets/Scripts/UI/Effect/Effect_DigitalSprite.cs
+++ Assets/Scripts/UI/Effect/Effect_DigitalSprite.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using CarterGames.Assets.AudioManager;
 
 public class Effect_DigitalSprite : PPEffectBase
 {
     public GameObject mErrorObj = null;
+
+    public string openMusicID;
     public override void StartModify()
     {
         base.StartModify();
+
+        AudioManager.instance.StopBGMusic(openMusicID);
         mErrorObj.SetActive(true);
     }
 
