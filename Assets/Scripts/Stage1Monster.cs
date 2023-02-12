d3f70e8cad01f9f4a353c35928e6983957f425a5
aqwszx2@gmail.com <aqwszx2@gmail.com>
2023-02-12 21:58:19 +0800

    [Dim] MonsterLevel1 prefab

--- Assets/Scripts/Stage1Monster.cs
+++ Assets/Scripts/Stage1Monster.cs
@@ -4,27 +4,38 @@ using UnityEngine;
 
 public class Stage1Monster : MonoBehaviour
 {
-    
-
     private float dashingTime = 0.2f;
     private float targetY = 0f;
+    private float originTargetY = 0f;
+    private Vector3 originPosition;
+    private float moveTime = 0f;
 
     private AudioSource MonsterAudio;
+    private GameObject player;
+    private bool onAtk = false;
 
     [SerializeField] public AudioClip scareAudio;
-    [SerializeField] private Animation moveShake;
+    [SerializeField] public AudioClip showupAudio;
+    [SerializeField] private Animator animControl;
+    [SerializeField] private float maxDistanceToPlayer = 12f;
     [SerializeField] private float movingPower = 12f;
     [SerializeField] private float moveDistance = 1f;
+    [SerializeField] private float atkRange = 7f;
+    [SerializeField] private float atkDetectionRange = 12f;
+    [SerializeField] private float moveInterval = 1f;
 
 
     private void Awake()
     {
-        MonsterAudio = GetComponent<AudioSource>();
+        //MonsterAudio = GetComponent<AudioSource>();
+        player = GameObject.Find("Player");
     }
     // Start is called before the first frame update
     void Start()
     {
-
+        originTargetY = transform.position.y;
+        targetY = originTargetY;
+        originPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -35,22 +46,73 @@ public class Stage1Monster : MonoBehaviour
 
     private void FixedUpdate()
     {
-        transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, targetY), movingPower * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, targetY, transform.position.z), movingPower * Time.deltaTime);
+        if (!onAtk)
+        {
+            if (Time.time > moveTime)
+            {
+                moveTime += moveInterval;
+                MonsterMoveUp();
+            }
+            CheckPlayerOnRange();
+        }
     }
 
     public void ResetPosition()
     {
-        targetY = 0f;
-        transform.position = new Vector2(0f, 0f);
+        targetY = originTargetY;
+        transform.position = originPosition;
     }
     public void MonsterMoveUp()
     {
         targetY = transform.position.y + moveDistance;
-        moveShake.Play();
+        //moveShake.Play("moveUp");
+        //MonsterScare(scareAudio);
     }
 
-    void MonsterScare()
+    public void MonsterShowUp()
     {
+        targetY = transform.position.y + 2.3842f;
+        //moveShake.Play("showup");
+        //MonsterScare(showupAudio);
+    }
+
+    public void MonsterAtk()
+    {
+        onAtk = true;
+        animControl.SetTrigger("Atk");
+        //targetY = transform.position.y + 2.3842f;
+        //moveShake.Play("showup");
+        //MonsterScare(showupAudio);
+        SimpleTimerManager.instance.RunTimer(CheckPlayerBeAttacked, 0.6f);
+        SimpleTimerManager.instance.RunTimer(restAtk, 1f);
+    }
+
+    private void restAtk()
+    {
+        onAtk = false;
+    }
+
+    private void CheckPlayerOnRange()
+    {
+        if(player.transform.position.y - transform.position.y < atkDetectionRange)
+        {
+            MonsterAtk();
+        }
+    }
+
+    private void CheckPlayerBeAttacked()
+    {
+        if (player.transform.position.y - transform.position.y < atkRange)
+        {
+            //player die
+        }
+    }
+
+
+    void MonsterScare(AudioClip clip)
+    {
+        MonsterAudio.clip = clip;
         if (MonsterAudio.clip)
         {
             MonsterAudio.Play();
